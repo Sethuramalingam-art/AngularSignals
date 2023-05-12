5dff5b59c99076358f5f98b841ebeac528d37e0b
Sethuramalingam Duraisamy <sethuramalingam.d@encora.com>
2023-05-12 23:11:07 +0530

    main: signals project setup

--- app-signals/src/app/app.component.ts
+++ app-signals/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app-signals';
+  search = '';
+  users = [
+    { id: 1, name: 'sethu' },
+    { id: 2, name: 'saran' },
+  ];
+  filterUsers = this.users;
+
+  setSearchString(e: Event) {
+    this.search = (e.target as HTMLInputElement).value;
+    this.filterUsers = this.users.filter((user) =>
+      user.name.startsWith(this.search)
+    );
+  }
+
+  addUser() {
+    this.users = [...this.users, { id: 3, name: 'magi' }];
+    this.filterUsers = this.users;
+  }
 }
